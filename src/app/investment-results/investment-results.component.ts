211d19a58ca8fc74050aebdf5e91a4c8c12c6cb3
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-07-09 21:43:09 -0500

    Module Failure

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,14 +1,8 @@
 import {Component, computed, inject} from '@angular/core';
-import {CurrencyPipe, DatePipe} from "@angular/common";
 import {InvestmentService} from "../service/investment.service";
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [
-    DatePipe,
-    CurrencyPipe
-  ],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.scss'
 })
